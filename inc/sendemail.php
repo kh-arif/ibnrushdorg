96f73cf18f2b7d80c7d69b59f4320e6d3a4b66e5
khalid <khalid@pop-os.localdomain>
2019-10-11 00:54:00 +0300

    add server.php

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -1,10 +1,10 @@
 <?php
 
-    $to = "khamuqbil@gmailcom";
+    $to = "@gmailcom";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
-    $subject = "You have a message from your BizPro";
+    $subject = "هنا تكتب عنوان الرسالة";
 
     $fields = array();
     $fields{"Fname"}    = "First Name";
@@ -14,7 +14,7 @@
     $fields{"message"}   = "Message";
 
 
-    $body = "Here is the message you got:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $body = "هنا التي ترسل إليك\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
     $send = mail($to, $subject, $body, $headers);
 
