3879466930612ef07b1685b64f35896f27a756dd
kh-arif <khalid7380@hotmail.com>
2021-01-07 09:14:41 +0300

    change the time zone

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 STATUS = (
     (0,"Drafst"),
@@ -11,9 +11,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now = True)
+    updated_on = models.DateTimeField(default = timezone.now)
     content = models.TextField()
-    created_on =models.DateTimeField(auto_now = True)
+    created_on =models.DateTimeField(default = timezone.now)
     status = models.IntegerField(choices=STATUS, default=0)
 
 
