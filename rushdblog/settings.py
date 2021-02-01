00333e2c428f29596c985d4330228b8863f27da5
kh-arif <khalid7380@hotmail.com>
2021-02-01 05:27:39 +0300

    www

--- rushdblog/settings.py
+++ rushdblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'pg=-zoc#)3&w0=8f%u^z8fk_gnfsh=pmn!tr881iojy=yy(@v+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','rushd-org.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','rushd-org.herokuapp.com','www.rushdsc.org']
 
 
 # Application definition
