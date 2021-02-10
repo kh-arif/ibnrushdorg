76fb80ccc77d0c2b24efedeca73b11e75bd4d1be
kh-arif <khalid7380@hotmail.com>
2021-02-11 01:18:29 +0300

    medicalpost url

--- blog_medical/urls.py
+++ blog_medical/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path 
 
 urlpatterns = [
-    path('medicalpost', views.MedicalPostList, name='medica_blog'),
+    path('medicalpost', views.MedicalPostList.as_view(), name='medica_blog'),
 ]
 
 
